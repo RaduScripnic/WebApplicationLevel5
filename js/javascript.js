ec56e7721cd5a46be452ad76c97d7ef483265555
GitforALL <68301642+RaduScripnic@users.noreply.github.com>
2025-05-09 20:24:59 +0100

    added the sections comments, fixed the carusel on the projects page and added the css-validation

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,2 @@
 let currentIndex = 0;
 console.log("JavaScript file is linked correctly!");
-function moveSlide(direction) {
-    const slides = document.querySelectorAll(".carousel-slide img");
-    const totalSlides = slides.length;
-    const carousel = document.querySelector(".carousel");
-
-    currentIndex += direction;
-
-    // Loop back to the first image if we go past the last one
-    if (currentIndex >= totalSlides) {
-        currentIndex = 0;
-    }
-    // Loop to the last image if we go before the first one
-    if (currentIndex < 0) {
-        currentIndex = totalSlides - 1;
-    }
-
-    // Adjust the transform property to move the slides
-    carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
